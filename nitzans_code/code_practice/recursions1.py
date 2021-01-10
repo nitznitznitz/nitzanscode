8cf79cc6193fbb36797baa55021c93dec4a72add
Nitzan <nitz.brand@gmail.com>
2021-01-10 11:38:49 +0200

    comment

--- nitzans_code/code_practice/recursions1.py
+++ nitzans_code/code_practice/recursions1.py
@@ -1,12 +1,15 @@
 # FUNCTIONS
 def list_sum(numbers_list):
     """Receives list of numbers, returns their sum"""
-    return sum(numbers_list)
+    if len(numbers_list) == 1:
+        return numbers_list[0]
+    else:
+        return numbers_list[0] + list_sum(numbers_list[1:])
 
 
 # MAIN
 def main():
-    list_of_numbers = [1, 2, 3]
+    list_of_numbers = [1, 2, 3, 4]
     print list_sum(list_of_numbers)
 
 
